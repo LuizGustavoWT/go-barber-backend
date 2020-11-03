5d7be41a1963b5cba3d7b87c4faab284e8415de5
LuizGustavoWT <gustavoweberthums@hotmail.com>
2020-11-02 23:42:06 -0300

    Primeiro projeto com node concluido

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,21 +1,37 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
+    const parsedDate = parseISO(date);
 
-  appointments.push(appointment);
-  return response.json(appointment);
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
 });
 
 export default appointmentsRouter;
